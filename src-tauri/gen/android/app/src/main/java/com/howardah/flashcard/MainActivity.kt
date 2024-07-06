5554139ce11db66bc6668faeeeac4ed6fbb5682f
Adam Howard <adam.thomas.howard@gmail.com>
2024-07-06 12:18:36 +0200

    Init ads

--- src-tauri/gen/android/app/src/main/java/com/howardah/flashcard/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/howardah/flashcard/MainActivity.kt
@@ -1,3 +1,23 @@
 package com.howardah.flashcard
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.AdSize
+
+class MainActivity : TauriActivity() {
+    private lateinit var adView: AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MobileAds.initialize(this) {}
+
+        adView = AdView(this)
+        adView.adSize = AdSize.BANNER
+        adView.adUnitId = "ca-app-pub-9050765895627837/7742513512"
+
+        val adRequest = AdRequest.Builder().build()
+        adView.loadAd(adRequest)
+    }
+}
